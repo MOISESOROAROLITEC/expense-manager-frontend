c3f9effe60c5081737e64e2fe2671ede1666e658
moisesoroarolitec <moisesoro@arolitec.com>
2023-07-26 12:52:53 +0000

    fix activate button when user use url to assecc to dashboard

--- src/app/dashboard/nav/navigation.tsx
+++ src/app/dashboard/nav/navigation.tsx
@@ -1,47 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../../images/logo.png";
 import "./navigation.scss";
-import { Link } from "react-router-dom";
 
-interface navOptions {
+interface navOptionsInterface {
   text: string;
   href: string;
   icon: string;
 }
 
+const navigationOptions: navOptionsInterface[] = [
+  {
+    text: "Epargne",
+    href: "/dashboard/saving",
+    icon: "payments",
+  },
+  {
+    text: "Historique des transactions",
+    href: "/dashboard/transactions",
+    icon: "history",
+  },
+  {
+    text: "Progression collective",
+    href: "/dashboard/colective-progression",
+    icon: "bar_chart_4_bars",
+  },
+  {
+    text: "Apprendre",
+    href: "/dashboard/learn",
+    icon: "local_library",
+  },
+  {
+    text: "Progression ",
+    href: "/dashboard/colective-progression",
+    icon: "bar_chart_4_bars",
+  },
+  {
+    text: "Preferences",
+    href: "/dashboard/settings",
+    icon: "settings",
+  },
+];
+
 const Navigation: React.FC = () => {
-  const options: navOptions[] = [
-    {
-      text: "Epargne",
-      href: "/dashboard/saving",
-      icon: "payments",
-    },
-    {
-      text: "Historique des transactions",
-      href: "/dashboard/transactions",
-      icon: "history",
-    },
-    {
-      text: "Progression collective",
-      href: "/dashboard/colective-progression",
-      icon: "bar_chart_4_bars",
-    },
-    {
-      text: "Apprendre",
-      href: "/dashboard/learn",
-      icon: "local_library",
-    },
-    {
-      text: "Progression ",
-      href: "/dashboard/colective-progression",
-      icon: "bar_chart_4_bars",
-    },
-    {
-      text: "Preferences",
-      href: "/dashboard/settings",
-      icon: "settings",
-    },
-  ];
+  const [navButtonActivatedIndex, setNavButtonActivatedIndex] = useState(0);
+
+  useEffect(() => {
+    const url = window.location.href;
+    for (let index = 0; index < navigationOptions.length; index++) {
+      if (url.includes(navigationOptions[index].href)) {
+        setNavButtonActivatedIndex(index);
+      }
+    }
+  }, []);
+
+  const activateNavElement = (el: number) => {
+    navigationOptions.forEach((_, index) => {
+      document
+        .querySelector(`#button_${index}`)
+        ?.classList.remove("activate-nav-list-btn");
+    });
+    document
+      .querySelector(`#button_${el}`)
+      ?.classList.add("activate-nav-list-btn");
+  };
   return (
     <div className="green-1 h-100 w-100 elevation-1 rounded-4 navigation">
       <div className="w-100 d-flex justify-content-center app-logo">
@@ -49,11 +71,17 @@ const Navigation: React.FC = () => {
       </div>
       <div className="hr green-3 my-3"></div>
       <div className="w-100 options">
-        {options.map((option) => (
+        {navigationOptions.map((option, index) => (
           <Link
-            className="w-100 d-block rounded-4 green-1 d-flex align-items-center text-decoration-none option"
+            className={`${
+              index === navButtonActivatedIndex && "activate-nav-list-btn"
+            } w-100 d-block rounded-4  green-1 d-flex align-items-center text-decoration-none option`}
             key={option.text}
+            id={"button_" + index}
             to={option.href}
+            onClick={() => {
+              activateNavElement(index);
+            }}
           >
             <span className="material-symbols-rounded fw-light">
               {option.icon}{" "}
