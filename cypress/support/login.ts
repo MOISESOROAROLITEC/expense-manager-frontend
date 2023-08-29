5e5635bd862193fe89159f01eb4e834c86e4f93b
moisesoroarolitec <moisesoro@arolitec.com>
2023-08-29 20:18:48 +0000

    delete unuse files

--- cypress/support/login.ts
+++
@@ -1,6 +0,0 @@
-import { login } from "../../src/app/shared/utilities/request";
-
-const email = process.argv[2];
-const password = process.argv[3];
-
-login(email, password);
--- cypress/support/sign-up.ts
+++
@@ -1,8 +0,0 @@
-import { signup } from "../../src/app/shared/utilities/request";
-
-const username = process.argv[2];
-const email = process.argv[3];
-const password = process.argv[4];
-const birthDay = new Date().toISOString();
-
-signup({ name: username, email, password, birthDay });
